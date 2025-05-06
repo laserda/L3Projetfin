1588c96accbd097c13eecb83dc74858ba3071e22
laserda <komanatsefabriceigor@gmail.com>
2025-05-06 18:57:34 -0400

    reorganisation du projet

--- drizzle/0002_bitter_madame_web.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE "agent" ALTER COLUMN "created_at" SET DEFAULT now();--> statement-breakpoint
-ALTER TABLE "agent" ALTER COLUMN "created_at" DROP NOT NULL;--> statement-breakpoint
-ALTER TABLE "agent" ALTER COLUMN "updated_at" SET DEFAULT now();--> statement-breakpoint
-ALTER TABLE "agent" ALTER COLUMN "updated_at" DROP NOT NULL;
